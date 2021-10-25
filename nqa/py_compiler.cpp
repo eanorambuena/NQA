d00f81cf892db0a8fb7aae77e241afa7940473c8
eanorambuena <eanorambuena@uc.cl>
2021-10-25 18:51:03 -0300

    Test in path PASSED

--- nqa/py_compiler.cpp
+++ nqa/py_compiler.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main(){
 
-    system("py -m PyInstaller --onefile --name \"nqa\" \"C:\\Users\\Alumno\\NQA\nqa\\__main__.py\"");
+    system("py -m PyInstaller --onefile --name \"nqa\" \"C:\\Users\\Alumno\\NQA\\nqa\\__main__.py\"");
     
     return 0;
 }
+
+// py -m PyInstaller --onefile --name "nqa" "C:\Users\Alumno\NQA\nqa\__main__.py"
